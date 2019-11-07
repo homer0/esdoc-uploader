3ed3ac03af34f5aae3783be115c207e9d9f6371a
homer0 <leo.apiwan@olapic.com>
2019-11-06 23:10:48 -0300

    refactor: update the syntax and the documentation

--- src/index.js
+++ src/index.js
@@ -2,30 +2,41 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 /**
+ * @typedef {Function} UploadCallback
+ * @param {Boolean} success Whether the documentation was uploaded or not.
+ * @param {?String} url     The url for the documentation.
+ */
+
+/**
+ * @typedef {Function} RequestCallback
+ * @param {?Error} error    In case the request fails.
+ * @param {String} response The request response.
+ * @param {Number} status   The response status code.
+ * @ignore
+ */
+
+/**
 * ESDocUploader, connects with the [ESDoc hosting service](https://doc.esdoc.org/) API in order
 * to generage the documentation for your project.
-* @version  1.0.0
 */
 class ESDocUploader {
   /**
-   * Create a new instance of the uploader.
-   * @param  {String} [url=null] - This is the GitHub repository url. The required format its
-   *                               `git@github.com:[author]/[repository].git`. You can also
-   *                               ignore it and it will automatically search for it on your
-   *                               `package.json`.
-   * @public
+   * @param {?String} [url=null] This is the GitHub repository url. The required format its
+   *                             `git[at]github.com:[author]/[repository].git`. You can also
+   *                             ignore it and it will automatically search for it on your
+   *                             `package.json`.
    */
   constructor(url = null) {
     /**
      * A list of pre defined messages that the class will log.
      * @type {Object}
-     * @private
+     * @protected
      * @ignore
      */
     this._messages = {
-      constructor: 'The repository url is invalid. ' +
-      'There is likely additional logging output above',
-      invalidUrl: 'The repository url is invalid. You can\'t upload anything',
+      invalidUrl: 'The repository url is invalid',
+      invalidPackageUrl: 'The repository url is invalid. ' +
+        'There is likely additional logging output above',
       uploading: 'The documentation is already being uploaded',
       unexpected: 'Unexpected error, please try again',
       noPackage: 'There\'s no package.json in this directory',
@@ -36,119 +47,111 @@ class ESDocUploader {
       success: 'The documentation was successfully uploaded:',
     };
     /**
-     * The repository url. It can be null if the one provided is not valid or if there isn't
-     * one on the `package.json`.
-     * @type {string|null}
-     * @private
+     * The repository url. It can be `null` if the one provided via the parameter is invalid or
+     * if a valid one can't be retrieved from the `package.json`.
+     * @type {?String}
+     * @protected
      * @ignore
      */
-    this.url = url === null ?
-      this._retrieveUrlFromPackage() :
-      this._validateUrl(url);
+    this.url = url === null ? this._retrieveUrlFromPackage() : this._validateUrl(url);
     /**
-     * A flag to know if the class it's currently uploading something.
+     * A flag to know if the class it's currently uploading the documentation or not.
      * @type {Boolean}
-     * @private
+     * @protected
      * @ignore
      */
     this._uploading = false;
     /**
      * A small dictionary used to store information relative to the ESDoc API, like it's
-     * main domain or the path to create a new doc.
+     * main hostname and the path to create a new documentation.
+     * When a new documentation is created, this object will be updated with the path
+     * to check if the documentation is ready.
      * @type {Object}
-     * @private
+     * @protected
      * @ignore
      */
     this._api = {
-      host: 'https://doc.esdoc.org',
+      host: 'doc.esdoc.org',
       create: '/api/create',
     };
     /**
-     * The name of the file where the class it's going to check if the docs were uploaded.
+     * The name of the file where the class it's going to check if the docs were uploaded. The
+     * complete path is created with the information from the response the class gets when a
+     * new documentation is created.
      * @type {String}
-     * @private
+     * @protected
      * @ignore
      */
     this._finishFile = '/.finish.json';
     /**
-     * The amount of time the class will wait between checks to see if the docs site was
-     * generated.
+     * The interval time the class will use in order to check if an uploaded documentation
+     * is available or not.
      * @type {Number}
-     * @private
+     * @protected
      * @ignore
      */
     this._intervalTime = 4000;
     /**
-     * After the first request, this is where the returned path for the docs on the server
-     * will be stored.
-     * @type {String}
-     * @private
-     * @ignore
-     */
-    this._path = '';
-    /**
-     * A callback that will be executed after confirmation that the docs were generated.
-     * @type {Function}
-     * @private
+     * A callback that will be executed after getting a confirmation that the documentation
+     * was successfully updated. It's value is set using the `upload` method.
+     * @type {?UploadCallback}
+     * @protected
      * @ignore
      */
     this._callback = null;
     /**
-     * The text that will show up on the terminal.
+     * The text that will show up on the console.
      * @type {String}
-     * @private
+     * @protected
      * @ignore
      */
     this._indicatorText = 'Uploading';
     /**
      * The amout of time in which the indicator will be updated.
      * @type {Number}
-     * @private
+     * @protected
      * @ignore
      */
     this._indicatorInterval = 1000;
     /**
-     * A utility counter to know how many dos will be added to the indicator
+     * A utility counter to know how many dots will be added to the indicator.
      * @type {Number}
-     * @private
+     * @protected
      * @ignore
      */
     this._indicatorCounter = -1;
     /**
-     * After this many iterations, the dots will start to be removed instead of added. When the
-     * counter hits 0, it will start adding again, until it hits this limit.
+     * After this many iterations, the dots in the indicator will start to be removed instead
+     * of being added. When the counter hits 0, it will start adding again, until it
+     * hits this limit.
      * @type {Number}
-     * @private
+     * @protected
      * @ignore
      */
     this._indicatorLimit = 3;
     /**
      * A flag to know if the indicator it's currently adding dots or removing them.
      * @type {Boolean}
-     * @private
+     * @protected
      * @ignore
      */
     this._indicatorIncrease = true;
     /**
-     * If there's no url, log the error message.
+     * @ignore
      */
-    if (this.url === null) {
-      this._logError('constructor');
-    }
+    this._ask = this._ask.bind(this);
   }
   /**
-   * After the class is istantiated, this method can be used to check if the url is valid and
-   * if the method `upload` can be called
-   * @public
+   * Checks whether the repository is valid and the class can start uploading the documentation.
+   * @return {Boolean}
    */
   canUpload() {
-    return this.url !== null;
+    return !!this.url;
   }
   /**
    * Upload your documentation to the ESDoc API.
-   * @param  {Function} [callback=() => {}] - An optional callback to be executed after
-   *                                          everthing is ready.
-   * @public
+   * @param {UploadCallback} [callback=() => {}] An optional callback to be executed after
+   *                                             everthing is ready.
    */
   upload(callback = () => {}) {
     if (this.url === null) {
@@ -172,8 +175,8 @@ class ESDocUploader {
               response;
 
             if (useResponse.success) {
-              this._setAPIUrl('path', useResponse.path);
-              this._setAPIUrl(
+              this._setAPIPath('path', useResponse.path);
+              this._setAPIPath(
                 'status',
                 `${useResponse.path}${this._finishFile}`
               );
@@ -187,18 +190,16 @@ class ESDocUploader {
     }
   }
   /**
-   * Tries to retrieve the repository url from your `pacakge.json`.
-   * @return {String} The repository url that was on your `package.json`.
-   * @private
+   * Tries to retrieve the repository url from the project's `pacakge.json`.
+   * @return {String}
+   * @protected
    * @ignore
    */
   _retrieveUrlFromPackage() {
     const packagePath = path.resolve('./package.json');
     const packageContents = fs.readFileSync(packagePath, 'utf-8');
     let result = null;
-    if (!packageContents) {
-      this._logError('noPackage');
-    } else {
+    if (packageContents) {
       const authorAndRepoParts = 2;
       const property = JSON.parse(packageContents).repository;
       if (!property) {
@@ -218,16 +219,22 @@ class ESDocUploader {
         const repository = urlParts[urlParts.length - 1];
         result = this._buildUrl(author, repository);
       }
+    } else {
+      this._logError('noPackage');
+    }
+
+    if (result === null) {
+      this._logError('invalidPackageUrl');
     }
 
     return result;
   }
   /**
    * Generates a new url with the required format to use with the ESDoc API.
-   * @param  {String} author     - The GitHub username.
-   * @param  {String} repository - The repository name.
-   * @return {String} The new url, on the required format for ESDoc.
-   * @private
+   * @param {String} author     The GitHub username.
+   * @param {String} repository The repository name.
+   * @return {String}
+   * @protected
    * @ignore
    */
   _buildUrl(author, repository) {
@@ -240,32 +247,36 @@ class ESDocUploader {
   }
   /**
    * Validates a given url to see if it has the required format by the ESDoc API.
-   * @param  {String} url - The url to validate.
-   * @return {String|null} If the url it's valid, it will return it, otherwise, itw will
-   * return null.
-   * @private
+   * @param {String} url - The url to validate.
+   * @return {?String} If the url it's valid, it will return it, otherwise, it will
+   *                   return `null`.
+   * @protected
    * @ignore
    */
   _validateUrl(url) {
     let result = null;
     if (url.match(/^git@github\.com:[\w\d._-]+\/[\w\d._-]+\.git$/)) {
       result = url;
+    } else {
+      this._logError('invalidUrl');
     }
 
     return result;
   }
   /**
    * This method is called after the initial request to the API, and tells the class to check
-   * every X seconds to see if the documentation was uploaded.
-   * @private
+   * every X milliseconds to see if the documentation was uploaded.
+   * @protected
    * @ignore
    */
   _startAsking() {
-    setTimeout(this._ask.bind(this), this._intervalTime);
+    setTimeout(this._ask, this._intervalTime);
   }
   /**
-   * It makes a request to check if the documentation was uploaded or not.
-   * @private
+   * It makes a request to check if the documentation was uploaded or not. If is not ready, it
+   * will call `_startAsking` to setup a new check; otherwise, it will invoke the callback sent
+   * to `upload`.
+   * @protected
    * @ignore
    */
   _ask() {
@@ -284,34 +295,33 @@ class ESDocUploader {
   }
   /**
    * This method is called after it's confirmed that the documentation was successfully uploaded,
-   * and it stops teh indicator, logs a mesage with the url for the documetation and invokes the
+   * it stops the indicator, logs a message with the url for the documetation and invokes the
    * callback set in the `upload()` method.
-   * @private
+   * @protected
    * @ignore
    */
   _finish() {
     this._uploading = false;
     this._stopIndicator();
-    const docUrl = this._getAPIUrl('path');
+    const url = this._getAPIUrl('path');
     // eslint-disable-next-line no-console
     console.log(
       '\x1b[30m[%s] \x1b[32m%s\x1b[0m',
       new Date(),
-      `${this._messages.success} ${docUrl}`
+      `${this._messages.success} ${url}`
     );
-    this._callback(true, docUrl);
+    this._callback(true, url);
   }
   /**
-   * Returns a url for the ESDoc API.
-   * @param  {String} type - The type of url you need. This is parameter it's the key for the
-   *                         `_api` dictionary.
-   * @return {String} It will return the API domain and the value in the `_api` dictionary for
-   *                  given type.
-   * @private
+   * Returns a complete url for the ESDoc API.
+   * @param  {String} apiPath The reference name for the path the request is for,
+   *                          inside the `_api` dictionary.
+   * @protected
    * @ignore
    */
-  _getAPIUrl(type) {
-    return this._api.domain + this._api[type];
+  _getAPIUrl(apiPath) {
+    const usePath = this._api[apiPath];
+    return `https://${this._api.domain}${usePath}`;
   }
   /**
    * Makes a POST request to the API.
@@ -319,7 +329,7 @@ class ESDocUploader {
    *                                    inside the `_api` dictionary.
    * @param {Object}          body      The body of the request.
    * @param {RequestCallback} callback  The callback to be invoked when the request is finished.
-   * @private
+   * @protected
    * @ignore
    */
   _postRequest(apiPath, body, callback) {
@@ -343,7 +353,7 @@ class ESDocUploader {
    * @param {String}          apiPath   The reference name for the path the request is for,
    *                                    inside the `_api` dictionary.
    * @param {RequestCallback} callback  The callback to be invoked when the request is finished.
-   * @private
+   * @protected
    * @ignore
    */
   _getRequest(apiPath, callback) {
@@ -362,7 +372,7 @@ class ESDocUploader {
    * @param {Object}           reqOptions  The options for `https.request`.
    * @param {RequestCallback}  callback    The callback to be invoked when the request is
    *                                       finished.
-   * @private
+   * @protected
    * @ignore
    */
   _createAPIRequest(reqOptions, callback) {
@@ -396,23 +406,23 @@ class ESDocUploader {
     });
   }
   /**
-   * Set a new type of urlf or the ESDoc API. For example, the first request will return a
-   * relative path for the documentation, this class will use this method to save this path
-   * so it can be later be retrieved using `_getAPIUrl` and it wil already have the API main
-   * domain.
-   * @param {String} type - An identifier for your url.
-   * @param {String} url  - The relative url you want to save.
-   * @private
+   * Sets a new path reference to be used with the ESDoc API.
+   * After triggering the upload, this will be used to store the path the API uses so the class
+   * can check if the documentation is available.
+   * @param {String} name    A reference identifier for the path.
+   * @param {String} apiPath The relative path you want to save.
+   * @protected
    * @ignore
    */
-  _setAPIUrl(type, url) {
-    this._api[type] = url;
+  _setAPIPath(name, apiPath) {
+    this._api[name] = apiPath;
   }
   /**
-   * Logs an eror message to the terminal.
-   * @param {String|Error} error - This can be the message you want to log, a key for the
-   *                               `_messages` dictionary or an `Error` object.
-   * @private
+   * Logs an eror message to the terminal and, if `upload` was ever call, it invokes the callback
+   * informing that the operation wasn't successful.
+   * @param {String|Error} error The message to log, a key for the `_messages` dictionary or
+   *                             an `Error` object.
+   * @protected
    * @ignore
    */
   _logError(error) {
@@ -431,7 +441,7 @@ class ESDocUploader {
   }
   /**
    * Starts showing the progress indicator on the terminal.
-   * @private
+   * @protected
    * @ignore
    */
   _startIndicator() {
@@ -443,7 +453,7 @@ class ESDocUploader {
   }
   /**
    * The actual method that shows the progress indicator on the terminal.
-   * @private
+   * @protected
    * @ignore
    */
   _runIndicator() {
@@ -469,7 +479,7 @@ class ESDocUploader {
   }
   /**
    * Removes the progress indicator from the terminal.
-   * @private
+   * @protected
    * @ignore
    */
   _stopIndicator() {
@@ -479,7 +489,7 @@ class ESDocUploader {
   /**
    * Removes everything on the current terminal line and sets the cursor to the initial
    * position.
-   * @private
+   * @protected
    * @ignore
    */
   _restartLine() {
@@ -489,7 +499,7 @@ class ESDocUploader {
   /**
    * Writes a message in the terminal.
    * @param {String} message - The text to write.
-   * @private
+   * @protected
    * @ignore
    */
   _print(message) {
